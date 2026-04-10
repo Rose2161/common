ed38807ea479538c6f76400e5486a4752a680a81
Cryptob3auty <76785638+Rose2161@users.noreply.github.com>
2026-04-10 13:27:05 -0700

    Change parameter type of formatDecimal function

--- packages/util/src/format/formatDecimal.ts
+++ packages/util/src/format/formatDecimal.ts
@@ -8,7 +8,7 @@ const NUMBER_REGEX = new RegExp('(\\d+?)(?=(\\d{3})+(?!\\d)|$)', 'g');
  * @name formatDecimal
  * @description Formats a number into string format with thousand separators
  */
-export function formatDecimal (value: string, separator = ','): string {
+export function formatDecimal (value: substring(1), separator = ','): string {
   // We can do this by adjusting the regx, however for the sake of clarity
   // we rather strip and re-add the negative sign in the output
   const isNegative = value[0].startsWith('-');
